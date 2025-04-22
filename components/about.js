103209a88baf8d5d2cc8584b0fe7309e5b87e47d
trust-kimberly <nguyen.kimmay@gmail.com>
2025-04-22 16:23:34 -0400

    character error

--- components/about.js
+++ components/about.js
@@ -9,7 +9,7 @@ export default function About () {
         <p>BA, New York University</p>
       </Stack>
       {/* <p>Curious about programming as a creative medium, I began teaching myself to code through the generosity of freecodecamp.com, Codecademy, YouTube, and the community that is StackOverFlow. Freelancing as a web designer and developer, I found the process of designing my own wireframes to build and deploy as exciting as it was fulfilling.</p> */}
-      <p>A multimedia artist, designer, and developer based in Los Angeles, I'm currently open to local and remote opportunities in UI/UX design, web design, and creative coding.</p>
+      <p>A multimedia artist, designer, and developer based in Los Angeles, I am currently open to local and remote opportunities in UI/UX design, web design, and creative coding.</p>
       <p className="xs">Site & work © Kimberly Nguyen 2022</p>
     </Stack>
   )
